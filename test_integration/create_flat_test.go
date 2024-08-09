1bb61e07cc78fc7796ebea69f4c2a06633a75c6e
Ropho <gon4arencko.r@inbox.ru>
2024-08-09 20:48:18 +0300

    fixed integration tests

--- test_integration/create_flat_test.go
+++ test_integration/create_flat_test.go
@@ -6,8 +6,11 @@ package testintegration
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 
+	"github.com/Ropho/avito-bootcamp-assignment/api"
 	"github.com/Ropho/avito-bootcamp-assignment/internal/service"
 	"github.com/Ropho/avito-bootcamp-assignment/test_integration/testhelpers"
 	"github.com/ozontech/allure-go/pkg/framework/provider"
@@ -17,6 +20,14 @@ import (
 
 type FlatCreateSuite struct {
 	suite.Suite
+}
+
+type createFlatResponse struct {
+	ID      int    `json:"id"`
+	HouseID int    `json:"house_id"`
+	Price   int    `json:"price"`
+	Rooms   int    `json:"rooms"`
+	Status  string `json:"status"`
 }
 
 func (s *FlatCreateSuite) BeforeAll(provider.T) {}
@@ -54,7 +65,21 @@ func (s *FlatCreateSuite) TestService_FlatCreateModerator(t provider.T) {
 	resp, err = client.Do(req)
 	require.NoError(t, err)
 	require.Equal(t, resp.StatusCode, http.StatusOK)
-	defer resp.Body.Close()
+
+	got, err := getFlatFromBody(resp.Body)
+	require.NoError(t, err)
+
+	expected := createFlatResponse{
+		HouseID: int(body.HouseID),
+		Price:   int(body.Price),
+		Rooms:   int(body.Rooms),
+		Status:  string(api.Created),
+	}
+
+	require.Equal(t, expected.HouseID, got.HouseID)
+	require.Equal(t, expected.Price, got.Price)
+	require.Equal(t, expected.Rooms, got.Rooms)
+	require.Equal(t, expected.Status, got.Status)
 
 }
 
@@ -91,7 +116,37 @@ func (s *FlatCreateSuite) TestService_FlatCreateClient(t provider.T) {
 	resp, err = client.Do(req)
 	require.NoError(t, err)
 	require.Equal(t, resp.StatusCode, http.StatusOK)
-	defer resp.Body.Close()
 
+	got, err := getFlatFromBody(resp.Body)
+	require.NoError(t, err)
+
+	expected := createFlatResponse{
+		HouseID: int(body.HouseID),
+		Price:   int(body.Price),
+		Rooms:   int(body.Rooms),
+		Status:  string(api.Created),
+	}
+
+	require.Equal(t, expected.HouseID, got.HouseID)
+	require.Equal(t, expected.Price, got.Price)
+	require.Equal(t, expected.Rooms, got.Rooms)
+	require.Equal(t, expected.Status, got.Status)
 }
 func (s *FlatCreateSuite) AfterAll(provider.T) {}
+
+func getFlatFromBody(body io.ReadCloser) (createFlatResponse, error) {
+
+	var resp createFlatResponse
+	bytes, err := io.ReadAll(body)
+	if err != nil {
+		return createFlatResponse{}, fmt.Errorf("failed to read body: [%w]", err)
+	}
+	defer body.Close()
+
+	err = json.Unmarshal(bytes, &resp)
+	if err != nil {
+		return createFlatResponse{}, fmt.Errorf("failed to unmarshal json struct: [%w]", err)
+	}
+
+	return resp, nil
+}
